963f08cf90cf74de7a0aa35cab3549cc833f638c
Unknown <tiwipabmin@gmail.com>
2017-06-19 03:06:42 +0700

    Auto stash before merge of "develop" and "origin/develop"
    add school model and school route

---
+++ route/schoolRoute.js
@@ -0,0 +1,19 @@
+var models = require('../models/School')
+var express = require('express')
+var router = express.Router()
+
+router.get('/school', (req, res) => {
+  models.School.findAll().then((school) => {
+    res.json(school)
+  })
+})
+
+router.get('/school/:id', (req, res) => {
+  models.School.findAll({
+    where: {
+      id: req.param.id
+    }
+  }).then((school) => {
+    res.json(school)
+  })
+})
