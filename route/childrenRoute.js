85e928f697b92e2d1a2ac735cf9edcdfae6eb345
Leconize <g.supavit@gmail.com>
2017-06-18 22:11:41 +0700

    add endpoints for children model

--- route/childrenRoute.js
+++ route/childrenRoute.js
@@ -1,9 +1,36 @@
 var models = require('../models')
 var express = require('express')
 var router = express.Router()
-router.get('/', (req, res) => {
-  models.Children.findAll().then(() => {
-    console.log(req.body)
+router.get('/children', (req, res) => {
+  models.Children.findAll().then((children) => {
+    res.json(children)
+  })
+})
+
+// /children/3
+router.get('/children/:id', (req, res) => {
+  models.Children.findAll({
+    where: {
+      id: req.params.id
+    }
+  }).then((child) => {
+    res.json(child)
+  })
+})
+
+router.post('/children', (req, res) => {
+  models.Children.create(req.body).then((child) => {
+    res.json(child)
+  })
+})
+
+router.delete('/children/:id', (req, res) => {
+  models.Children.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then((data) => {
+    res.json(data)
   })
 })
 
