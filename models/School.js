04d36024d3f04868e59264486700b6bcacc409eb
Unknown <tiwipabmin@gmail.com>
2017-06-20 13:36:54 +0700

    edit school model and school route

--- models/School.js
+++ models/School.js
@@ -2,9 +2,8 @@
 module.exports = function (sequelize, DataTypes) {
   var School = sequelize.define('School', {
     nameTh: DataTypes.STRING,
-    nameEng: DataTypes.STRING,
-    createdAt: DataTypes.DATE,
-    updatedAt: DataTypes.DATE
+    nameEng: DataTypes.STRING
+
   })
 
   return School
--- route/schoolRoute.js
+++ route/schoolRoute.js
@@ -9,18 +9,22 @@ router.get('/school', (req, res) => {
 })
 
 router.get('/school/:id', (req, res) => {
-  models.School.findAll({
-    where: {
-      id: req.param.id
+  models.School.findById(req.params.id).then((school) => {
+    if (school === null) {
+      res.status(404)
+      res.send({error: 'Not Found'})
+    } else {
+      res.json(school)
     }
-  }).then((school) => {
-    res.json(school)
   })
 })
 
 router.post('/school', (req, res) => {
   models.School.create(req.body).then((school) => {
     res.json(school)
+  }).catch((error) => {
+    res.status(404)
+    res.send({error: error.name})
   })
 })
 
@@ -31,6 +35,9 @@ router.delete('/school/:id', (req, res) => {
     }
   }).then((data) => {
     res.json(data)
+  }).catch((error) => {
+    res.status(400)
+    res.send({error: error.name})
   })
 })
 
